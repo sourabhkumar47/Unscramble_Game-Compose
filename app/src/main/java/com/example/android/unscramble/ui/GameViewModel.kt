69d618250642247a5b22b008841130e101b5ec75
Sourabh Kumar <20010001030@gateway.edu.in>
2023-04-26 22:34:15 +0530

    created shuffle and pick random fun

--- app/src/main/java/com/example/android/unscramble/ui/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/GameViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.android.unscramble.ui
 
 import androidx.lifecycle.ViewModel
+import com.example.android.unscramble.data.allWords
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -12,4 +13,26 @@ private val _uiState = MutableStateFlow(GameUiState())
 val iState: StateFlow<GameUiState> = _uiState.asStateFlow()
 
 class GameViewModel : ViewModel() {
+    private lateinit var currentWord: String
+    private var usedWords: MutableSet<String> = mutableSetOf()
+
+    private fun pickUpRandomWordAndShuffle(): String {
+        currentWord = allWords.random()
+
+        if (usedWords.contains(currentWord)) {
+            return pickUpRandomWordAndShuffle()
+        } else
+            usedWords.add(currentWord)
+        return pickUpRandomWordAndShuffle()
+    }
+
+    private fun shuffleCurrentWord(word: String): String {
+        val tempWord = word.toCharArray()
+        tempWord.shuffle()
+
+        while (String(tempWord) == word) {
+            tempWord.shuffle()
+        }
+        return String(tempWord)
+    }
 }

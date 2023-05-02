44596930f322831211614c0e91d5b706a4638cc3
Sourabh Kumar <20010001030@gateway.edu.in>
2023-05-03 01:25:57 +0530

    created Success path test

--- app/src/main/java/com/example/android/unscramble/ui/test/GameViewModelTest.kt
+++ app/src/main/java/com/example/android/unscramble/ui/test/GameViewModelTest.kt
@@ -1,5 +1,31 @@
 package com.example.android.unscramble.ui.test
 
+import com.example.android.unscramble.data.SCORE_INCREASE
+import com.example.android.unscramble.ui.GameViewModel
+import com.example.android.unscramble.ui.data.getUnscrambledWord
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Test
+
 class GameViewModelTest {
+    private val viewModel = GameViewModel()
+
+    @Test
+    fun gameViewModel_CorrectWordGuessed_ScoreUpdatedAndErrorFlagUnset() {
+        var currentGameUiState = viewModel.uiState.value
+        val correctPlayerWord = getUnscrambledWord(currentGameUiState.currentScrambledWord)
+        viewModel.updateUserGuess(correctPlayerWord)
+        viewModel.checkUserGuess()
+
+        currentGameUiState = viewModel.uiState.value
+        // Assert that checkUserGuess() method updates isGuessedWordWrong is updated correctly.
+        assertFalse(currentGameUiState.isGuessedWordWrong)
+        // Assert that score is updated correctly.
+        assertEquals(20, currentGameUiState.score)
+    }
+
+    companion object {
+        private const val SCORE_AFTER_FIRST_CORRECT_ANSWER = SCORE_INCREASE
+    }
 
 }

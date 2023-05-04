1c61399665fde382b62f2ebb365b2e3a27a9a00e
Sourabh Kumar <20010001030@gateway.edu.in>
2023-05-05 00:55:26 +0530

    created Boundary  path test

--- app/src/main/java/com/example/android/unscramble/ui/test/GameViewModelTest.kt
+++ app/src/main/java/com/example/android/unscramble/ui/test/GameViewModelTest.kt
@@ -6,6 +6,7 @@ import com.example.android.unscramble.ui.data.getUnscrambledWord
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
 import org.junit.Test
+import org.testng.Assert.assertNotEquals
 import org.testng.Assert.assertTrue
 
 class GameViewModelTest {
@@ -39,6 +40,19 @@ class GameViewModelTest {
         assertEquals(0, currentGameUiState.score)
         //assert that checkUserGuess() method updates isGuessedWordWrong correctly.
         assertTrue(currentGameUiState.isGuessedWordWrong)
+    }
+
+    //Boundary test case
+    @Test
+    fun gameViewModel_Initialization_FirstWordLoaded() {
+        val gameUiState = viewModel.uiState.value
+        val unScrambledWord = getUnscrambledWord(gameUiState.currentScrambledWord)
+
+        assertNotEquals(unScrambledWord, gameUiState.currentScrambledWord)
+        assertTrue(gameUiState.currentWordCount == 1)
+        assertTrue(gameUiState.score == 0)
+        assertFalse(gameUiState.isGuessedWordWrong)
+        assertFalse(gameUiState.isGameOver)
     }
 
     companion object {
